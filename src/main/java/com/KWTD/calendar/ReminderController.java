e2caad9f4d94b1b2a6715c8a045309b8a92f6f9e
Hriday Keswani <hridaykeswani@gmail.com>
2023-07-10 11:28:20 +0530

    implement meeting reminder system

--- src/main/java/com/KWTD/calendar/ReminderController.java
+++ src/main/java/com/KWTD/calendar/ReminderController.java
@@ -1,7 +1,10 @@
 package com.KWTD.calendar;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -10,8 +13,10 @@ import org.threeten.bp.LocalDate;
 
 import com.KWTD.meeting.Meeting;
 import com.KWTD.meeting.MeetingList;
+import com.KWTD.notifications.NotificationService;
 import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -25,11 +30,47 @@ public class ReminderController {
     
     /**
      * For checking for meetings and sending reminders
-     * an hour before one's schedule meeting time
+     * an hour before one's scheduled meeting time
      */
     @Scheduled(cron = "0 0 * * * *")
     public void sendReminder(){
-        //TODO: Using Firebase Cloud Messaging
+
+        DocumentReference dailyMeetings = database.collection("meetings+today")
+                                                .document(LocalDate.now().toString());
+        
+        try {
+            DocumentSnapshot snapshot = dailyMeetings.get().get();
+            MeetingList dailyMeetingList = new MeetingList();
+            if(snapshot.exists()){
+                dailyMeetingList = snapshot.toObject(MeetingList.class);
+                List<Meeting> meetings = dailyMeetingList.getSlots();
+                for(int i=0;i<meetings.size();i++){
+                    NotificationService service = new NotificationService();
+    
+                    LocalTime meetingTime = LocalTime.parse(meetings.get(i)
+                                                    .getMeetingTime());
+                    LocalTime timeNow = LocalTime.now();
+
+                    long timeDifference = timeNow.until(meetingTime, ChronoUnit.MINUTES);
+
+                    if(timeDifference<90 && timeDifference>0){
+                        // sends notification to mentee
+                        service.sendNotification("Meeting Reminder", 
+                            "You have an upcoming meeting at " + meetings.get(i).getMeetingTime() + ".",
+                            meetings.get(i).getMenteeNumber());
+
+                        // sends notification to mentor
+                         service.sendNotification("Meeting Reminder", 
+                            "You have an upcoming meeting at " + meetings.get(i).getMeetingTime() + ".",
+                            meetings.get(i).getMentorNumber());
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     
@@ -52,6 +93,8 @@ public class ReminderController {
                 }
 
                 List<Meeting> meetingsToday = new ArrayList<>();
+
+                // filling meeting list with meetings happening on a prticular day...
                 for(int i=0;i<meetingDocuments.size();i++){
                    for(int j=0;j<meetingDocuments.get(i).getSlots().size();j++){
                         if(meetingDocuments.get(i).getSlots()
@@ -60,9 +103,13 @@ public class ReminderController {
                         }
                    }
                 }
+
+                // writing meetings of the day on the database
                 DocumentReference ref = database.collection("meetings_today")
                                                 .document(LocalDate.now().toString());
+
                 MeetingList scheduleOfTheDay = new MeetingList();
+
                 scheduleOfTheDay.setSlots(meetingsToday);
                 ref.set(scheduleOfTheDay);
             }
